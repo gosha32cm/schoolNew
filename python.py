ff86f086a549b210a90b3247b4ea4ea2ac05d3a6
unknown <alina.leshchenko@infis.cz>
2025-03-11 09:27:50 +0100

    dasdasda

---
+++ python.py
@@ -0,0 +1,34 @@
+
+nazev = 80
+cislo = input("Zadejte cislo: ")
+print("vase cislo je "+cislo)
+#uzivatel zada cislo a mu to cislo se vrati zpatky
+
+cena = input("Zadejte cenu: ")
+cena = int(cena)
+ks = input("Zadejte pocet kusu: ")
+ks = int(ks)
+celkovaCena = cena*ks
+print("Celkova cena bude: ",celkovaCena)
+#pretypovani, uzivatel zada cenu za kus a pocet kusu a vypocita mu to celkovou cenu
+
+cislo = int(input("Zadejte vek: "))
+if cislo < 18:
+   print("je nezletily")
+elif cislo < 18 and cislo < 60:
+   print("je dospely")
+elif cislo < 60:
+   print("je duchodce")
+#podminky, uchazec zada vek a vypisu mu to do jake kategorii patri
+
+
+
+   
+   
+
+
+
+
+
+
+
